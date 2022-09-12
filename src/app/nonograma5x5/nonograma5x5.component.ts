843f8e1fc633976743e4fc4b2f76faf014c352b3
Lincoln Cruz <79378712+LincolnCFCruz@users.noreply.github.com>
2022-09-12 10:09:40 -0300

    Basic Logic Implementation

--- src/app/nonograma5x5/nonograma5x5.component.ts
+++ src/app/nonograma5x5/nonograma5x5.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nonograma5x5',
@@ -6,29 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nonograma5x5.component.css']
 })
 export class Nonograma5x5Component implements OnInit {
-  squares: string[];
+  squares: number[];
   xIsNext: boolean;
+  countError: number;
+  countPassUser: number;
+  countPassTotal: number;
 
-  constructor() { }
+
+  constructor(private _router: Router) { }
 
   ngOnInit(): void {
-    this.newGame();
+    this.newGame()
   }
 
   newGame() {
-    this.squares = Array(25).fill(null);
-    this.xIsNext = true;
+    this.squares = [1,0,0,0,0,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0]
+    this.countError = 0;
+    this.countPassTotal = 6;
+    this.countPassUser = 0;
   }
 
-  get player() {
-    return this.xIsNext ? 'X' : 'O';
+  verify(idx: number) {
+    if (this.squares[idx] == 1) {
+      this.countPassUser ++;
+    }
+    else{
+      window.alert("ERROU!");
+      this.countError ++;
+    }
+    this.calculateWinner()
   }
 
-  makeMove(idx: number) {
-    if (!this.squares[idx]) {
-      this.squares.splice(idx, 1, this.player);
-      this.xIsNext = !this.xIsNext;
+  calculateWinner(){
+    if (this.countPassUser == this.countPassTotal){
+      this._router.navigate(['/ganhou'])
+      }
+
+    if (this.countError >= 3){
+      if (confirm("Você perdeu, deseja reiniciar?"))
+        this.newGame()
+      else{
+        this._router.navigate(['/']);
+      }
     }
   }
-
 }
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -7,5 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 
 export class SquareComponent  {
-  @Input() value: 'X' | 'O';
+  @Input() value: boolean;
 }
